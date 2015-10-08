b9628f8970ae5fd53580d66f6d75c3423d0ce28f
jc321688 <fionnbharr.harkin@my.jcu.edu.au>
2015-10-08 20:40:05 +1000

    final site

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+
+var random_images_array = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg", "image13.jpg", "image14.jpg", "image15.jpg"];
+
+function getRandomImage(imgAr, path) {
+ path = path || 'Gallery/';
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
+
+function showImage(imgName) {
+     var curImage = document.getElementById('currentImg');
+     var thePath = 'Gallery/';
+     var theSource = thePath + imgName;
+	  curImage.src = theSource;
+     curImage.alt = imgName;
+     curImage.title = imgName;
+}
